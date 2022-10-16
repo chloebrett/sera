a8bbd866a755ed6aec7bb893051626051e356cac
Chloe Brett <cbre0013@student.monash.edu>
2022-10-16 13:26:51 +1100

    Filtering dropdowns

--- components/Filters.tsx
+++ components/Filters.tsx
@@ -1,5 +1,6 @@
-import { Dispatch, SetStateAction, useMemo } from "react";
-import { BestPractice, Cohort } from "../shared/sharedTypes";
+import { Dispatch, SetStateAction, useMemo } from 'react';
+import { BestPractice, Cohort } from '../shared/sharedTypes';
+import AutocompleteMultiSelect from './AutocompleteMultiSelect';
 
 const toggleSetter: <T>(
   setter: Dispatch<SetStateAction<Set<T>>>
@@ -30,80 +31,40 @@ interface Props {
 }
 
 const Filters = ({
-  filteredBestPractices,
   cohorts,
   filterCohorts,
   setFilterCohorts,
-  filterSubCohorts,
-  setFilterSubCohorts,
-  filterKeywords,
-  setFilterKeywords,
-  onClick
+  onClick,
 }: Props) => {
-  const toggleCohort = toggleSetter<string>(setFilterCohorts);
-  const toggleSubCohort = toggleSetter<string>(setFilterSubCohorts);
-  const toggleKeyword = toggleSetter<string>(setFilterKeywords);
-
-  const availableSubCohorts = useMemo(() =>  {
-    const subCohorts = new Set<string>();
-    filteredBestPractices.forEach((bestPractice) => bestPractice.subCohorts.forEach((subCohort) => subCohorts.add(subCohort)));
-    return subCohorts;
-  }, [filteredBestPractices]);
-
-  const availableKeywords = useMemo(() =>  {
-    const keywords = new Set<string>();
-    filteredBestPractices.forEach((bestPractice) => bestPractice.keywords.forEach((keyword) => keywords.add(keyword)));
-    return keywords;
-  }, [filteredBestPractices]);
-  
-  const cohortFilters = cohorts?.map((cohort) => (
-    <div key={cohort.referenceName}>
-      <input
-        type="checkbox"
-        checked={filterCohorts.has(cohort.referenceName)}
-        onChange={() => toggleCohort(cohort.referenceName)}
-        id={`checkbox-cohort-${cohort.referenceName}`}
-      />{" "}
-      <label htmlFor={`checkbox-cohort-${cohort.referenceName}`}>{cohort.name}</label>
-    </div>
-  ));
-
-  const subCohortFilters = Array.from(availableSubCohorts)?.map((subCohort) => (
-    <div key={subCohort}>
-      <input
-        type="checkbox"
-        checked={filterSubCohorts.has(subCohort)}
-        onChange={() => toggleSubCohort(subCohort)}
-        id={`checkbox-subcohort-${subCohort}`}
-      />{" "}
-      <label htmlFor={`checkbox-subcohort-${subCohort}`}>{subCohort}</label>
-    </div>
-  ));
-
-  const keywordFilters = Array.from(availableKeywords)?.map((keyword) => (
-    <div key={keyword}>
-      <input
-        type="checkbox"
-        checked={filterKeywords.has(keyword)}
-        onChange={() => toggleKeyword(keyword)}
-        id={`checkbox-keyword-${keyword}`}
-      />{" "}
-      <label htmlFor={`checkbox-keyword-${keyword}`}>{keyword}</label>
-    </div>
-  ));
+  const cohortFilters = (
+    <AutocompleteMultiSelect
+      possibleValues={cohorts.map(cohort => cohort.referenceName)}
+      chosenValues={Array.from(filterCohorts)}
+      setFieldValue={(_: any, values: string[]) => setFilterCohorts(new Set(values))}
+      fieldName="Cohorts"
+      fieldTitle="Cohorts"
+      freeSolo={false}
+      fullWidth={true}
+    />
+  );
 
   return (
     <div className="flex flex-col space-y-10">
       <div>
-        <h2 className="pb-4 font-bold">Cohorts (select any that are relevant to your research)</h2>
-        <div className="grid grid-cols-3 gap-4">{cohortFilters}</div>
-        
+        <h2 className="pb-4 font-bold">
+          Select any cohorts that are relevant to your research
+        </h2>
+        <div style={{ width: '100%' }}>{cohortFilters}</div>
       </div>
 
       <div className="flex justify-center pt-32">
-      <button className="px-4 py-2 font-semibold text-gray-800 border border-gray-800 rounded shadow bg-grey-800 hover:bg-gray-100 dark:border-white dark:text-white dark:hover:text-gray-800" onClick={onClick}>Find Best Practices</button>
+        <button
+          className="px-4 py-2 font-semibold text-gray-800 border border-gray-800 rounded shadow bg-grey-800 hover:bg-gray-100 dark:border-white dark:text-white dark:hover:text-gray-800"
+          onClick={onClick}
+        >
+          Find Best Practices
+        </button>
       </div>
-      
     </div>
   );
 };
--- components/SecondaryFilters.tsx
+++ components/SecondaryFilters.tsx
@@ -1,5 +1,6 @@
-import { Dispatch, SetStateAction, useMemo } from "react";
-import { BestPractice, Cohort } from "../shared/sharedTypes";
+import { Dispatch, SetStateAction, useMemo } from 'react';
+import { BestPractice, Cohort } from '../shared/sharedTypes';
+import AutocompleteMultiSelect from './AutocompleteMultiSelect';
 
 const toggleSetter: <T>(
   setter: Dispatch<SetStateAction<Set<T>>>
@@ -18,11 +19,16 @@ const toggleSetter: <T>(
 };
 
 export const practices: any = {
-  bestPractices: "best practices",
-  methodology: "methodology",
-  notesOfCaution: "notes of caution",
-  tools: "tools"
+  bestPractices: 'best practices',
+  methodology: 'methodology',
+  notesOfCaution: 'notes of caution',
+  tools: 'tools',
 };
+
+const practiceNameToKey: any = {};
+Object.keys(practices).forEach(key => {
+  practiceNameToKey[practices[key]] = key;
+})
 
 interface Props {
   filteredBestPractices: BestPractice[];
@@ -41,88 +47,108 @@ const SecondaryFilters = ({
   filterKeywords,
   setFilterKeywords,
   filterPractices,
-  setFilterPractices
+  setFilterPractices,
 }: Props) => {
-  const toggleSubCohort = toggleSetter<string>(setFilterSubCohorts);
-  const toggleKeyword = toggleSetter<string>(setFilterKeywords);
-  const togglePractice = toggleSetter<string>(setFilterPractices);
-
-  const availableSubCohorts = useMemo(() =>  {
+  const availableSubCohorts = useMemo(() => {
     const subCohorts = new Set<string>();
-    filteredBestPractices.forEach((bestPractice) => bestPractice.subCohorts.forEach((subCohort) => subCohorts.add(subCohort)));
+    filteredBestPractices.forEach((bestPractice) =>
+      bestPractice.subCohorts.forEach((subCohort) => subCohorts.add(subCohort))
+    );
     return subCohorts;
   }, [filteredBestPractices]);
 
-  const availableKeywords = useMemo(() =>  {
+  const availableKeywords = useMemo(() => {
     const keywords = new Set<string>();
-    filteredBestPractices.forEach((bestPractice) => bestPractice.keywords.forEach((keyword) => keywords.add(keyword)));
+    filteredBestPractices.forEach((bestPractice) =>
+      bestPractice.keywords.forEach((keyword) => keywords.add(keyword))
+    );
     return keywords;
   }, [filteredBestPractices]);
-  
+
   const subCohortIds = new Array<string>();
-  Array.from(availableSubCohorts)?.map((subCohort) => {subCohortIds.push(subCohort.replace(/\s/g,''))});
-  const subCohortFilters = Array.from(availableSubCohorts)?.map((subCohort, index) => (
-    <div key={subCohort}>
-      <input
-        type="checkbox"
-        checked={filterSubCohorts.has(subCohort)}
-        onChange={() => toggleSubCohort(subCohort)}
-        id={`checkbox-subcohort-${subCohortIds[index]}`}
-      />{" "}
-      <label htmlFor={`checkbox-subcohort-${subCohortIds[index]}`}>{subCohort} ({filteredBestPractices.filter((bp) => (bp.subCohorts.includes(subCohort))).length})</label>
-    </div>
-  ));
+  Array.from(availableSubCohorts)?.map((subCohort) => {
+    subCohortIds.push(subCohort.replace(/\s/g, ''));
+  });
+
+  const subCohortFilters = (
+    <AutocompleteMultiSelect
+      possibleValues={Array.from(availableSubCohorts)}
+      chosenValues={Array.from(filterSubCohorts)}
+      setFieldValue={(_: any, values: string[]) =>
+        setFilterSubCohorts(new Set(values))
+      }
+      fieldName="Sub-cohorts"
+      fieldTitle="Sub-cohorts"
+      freeSolo={false}
+      fullWidth={true}
+    />
+  );
 
   const keywordIds = new Array<string>();
-  Array.from(availableKeywords)?.map((keyword) => {keywordIds.push(keyword.replace(/\s/g,''))});
-  const keywordFilters = Array.from(availableKeywords)?.map((keyword, index) => (
-    <div key={keyword}>
-      <input
-        type="checkbox"
-        checked={filterKeywords.has(keyword)}
-        onChange={() => toggleKeyword(keyword)}
-        id={`checkbox-keyword-${keywordIds[index]}`}
-      />{" "}
-      <label htmlFor={`checkbox-keyword-${keywordIds[index]}`}>{keyword} ({filteredBestPractices.filter((bp) => (bp.keywords.includes(keyword))).length})</label>
-    </div>
-  ));
+  Array.from(availableKeywords)?.map((keyword) => {
+    keywordIds.push(keyword.replace(/\s/g, ''));
+  });
+
+  const keywordFilters = (
+    <AutocompleteMultiSelect
+      possibleValues={Array.from(availableKeywords)}
+      chosenValues={Array.from(filterKeywords)}
+      setFieldValue={(_: any, values: string[]) =>
+        setFilterKeywords(new Set(values))
+      }
+      fieldName="Keywords"
+      fieldTitle="Keywords"
+      freeSolo={false}
+      fullWidth={true}
+    />
+  );
 
   const practiceKeys = Object.keys(practices);
   const practiceIds = new Array<string>();
-  practiceKeys?.map((practice) => {practiceIds.push(practice.replace(/\s/g,''))});
-  const practiceFilters = practiceKeys?.map((practiceKey: string, index) => (
-    <div key={practiceKey}>
-      <input
-        type="checkbox"
-        checked={filterPractices.has(practiceKey)}
-        onChange={() => togglePractice(practiceKey)}
-        id={`checkbox-keyword-${practiceIds[index]}`}
-      />{" "}
-      <label htmlFor={`checkbox-keyword-${practiceIds[index]}`}>{practices[practiceKey]} ({filteredBestPractices.filter((bp) => practiceKey in bp).length})</label>
-    </div>
-  ));
+  practiceKeys?.map((practice) => {
+    practiceIds.push(practice.replace(/\s/g, ''));
+  });
 
-  filteredBestPractices.forEach((bp) => {console.log(bp.methodologyUsed)})
+  const practiceFilters = (
+    <AutocompleteMultiSelect
+      possibleValues={Array.from(practiceKeys).map(val => practices[val])}
+      chosenValues={Array.from(filterPractices).map(val => practices[val])}
+      setFieldValue={(_: any, values: string[]) =>
+        setFilterPractices(new Set(values.map(val => practiceNameToKey[val])))
+      }
+      fieldName="Content types"
+      fieldTitle="Content types"
+      freeSolo={false}
+      fullWidth={true}
+    />
+  );
+
+  filteredBestPractices.forEach((bp) => {
+    console.log(bp.methodologyUsed);
+  });
 
   return (
-    <div className="flex flex-col pb-5 space-y-10">
-      {subCohortFilters.length > 0 && 
+    <div className="flex flex-col pb-5 space-y-10" style={{ width: '40%' }}>
+      {availableSubCohorts.size > 0 && (
         <div>
-          <h2 className="pb-4 font-bold">Sub-cohorts</h2>
-          <div className="grid grid-cols-3 gap-4">{subCohortFilters}</div>
-        </div>}
+          <h2 className="pb-4 font-bold">Select sub-cohorts</h2>
+          {subCohortFilters}
+        </div>
+      )}
 
-      {keywordFilters.length > 0 && 
+      {availableKeywords.size > 0 && (
         <div>
-          <h2 className="pb-4 font-bold">Keywords</h2>
-          <div className="grid grid-cols-3 gap-4">{keywordFilters}</div>
-        </div>}
+          <h2 className="pb-4 font-bold">Select any relevant keywords</h2>
+          {keywordFilters}
+        </div>
+      )}
 
-      {practiceFilters.length > 0 && 
+      {practiceKeys.length > 0 && (
         <div>
-          <h2 className="pb-4 font-bold">Practices</h2>
-          <div className="grid grid-cols-3 gap-4">{practiceFilters}</div>
-        </div>}        
+          <h2 className="pb-4 font-bold">Select the types of content you want to see</h2>
+          {practiceFilters}
+        </div>
+      )}
     </div>
   );
 };
